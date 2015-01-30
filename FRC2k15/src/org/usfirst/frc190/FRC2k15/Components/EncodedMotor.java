0ae64019d2fe20331bfe17f72a58badb6cf5c42f
Patrick <pmurphy2@wpi.edu>
2015-01-30 17:08:03 -0500

    Part 1 of Encoder Speed calculations done

--- FRC2k15/src/org/usfirst/frc190/FRC2k15/Components/EncodedMotor.java
+++ FRC2k15/src/org/usfirst/frc190/FRC2k15/Components/EncodedMotor.java
@@ -7,27 +7,44 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
+/*
+1.72 in encoder shaft - 5.40 in cir 
+1.6 in wheel shaft - 5.03 in cir
+6 in wheels - 18.85 in cir 
 
+shaft is rotating at 5.03/5.40 times the speed of wheel
+0.93 times the speed
+9ft/s max
+9ft/s -> 9*12in/1ft = 108 in/s 
+108in/s * 1 revolution/18.85 in = 5.73 rev/s
+5.73rev/s (wheel) * 0.93 speed ratio (unitless) = 5.33 rev/s 
+5.33 rev/s (encoder) *1000pulse / rev
+5330 pulses/second at max speed
+
+18.85 in/rev (wheel) *.93 (wheelspeed/shaftspeed ratio) = 17.5 in/rev (encoder)
+17.5in/rev (encoder) *1rev/1000pulse = 0.0175 in/pulse
+ */
 public class EncodedMotor implements SpeedController {
 	private Encoder enc;
 	public SpeedController mtr;
 	private PIDController cnt;
-	private static final double distperpulse = 0.0001466;
+	private static final double distperpulse = 0.0175;//inches per pulse
 	public static ArrayList<EncodedMotor> mtrs = new ArrayList<EncodedMotor>();
 	private static boolean closedloop = true;
+	
 
 	public EncodedMotor(double Kp, double Ki, double Kd, double Kf,
-			Encoder source, SpeedController output, double period) {
+			Encoder source, SpeedController output, double period, double maxspd) {
 		cnt = new PIDController(Kp, Ki, Kd, Kf, source, output, period);
 		enc = source;
 		mtr = output;
-		init();
+		init(maxspd);
 	}
 
-	private void init() {
+	private void init(double maxspd) {
 		enc.setDistancePerPulse(distperpulse);
 		enc.setPIDSourceParameter(PIDSource.PIDSourceParameter.kRate);
-		cnt.setInputRange(-1.0, 1.0);
+		cnt.setInputRange(-maxspd, maxspd);
 		mtrs.add(this);
 		cnt.enable();
 	}
