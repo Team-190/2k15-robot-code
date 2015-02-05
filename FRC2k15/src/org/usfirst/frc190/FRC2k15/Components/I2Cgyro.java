5bd8ee4e48fa44854deba886cf6fcd5362605863
Patrick <pmurphy2@wpi.edu>
2015-02-05 15:45:39 -0500

    I2C gyro code updated

--- FRC2k15/src/org/usfirst/frc190/FRC2k15/Components/I2Cgyro.java
+++ FRC2k15/src/org/usfirst/frc190/FRC2k15/Components/I2Cgyro.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc190.FRC2k15.Components;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -12,15 +16,20 @@ public class I2Cgyro {
 	private short zRate;
 	private double ZRotateRate = 0;
 	private double zAngle = 0;
-	private double period = 0.02;// time in seconds between updates
+	private double period = 0.005;// time in seconds between updates
 	private final double sensitivity = 14.375; // 14.375 in ZRate = 1 degree /
 												// second
+	private long lasttime;
+	private double deadzone = 1.5;//anything absolute value less than deadzone is ignored
 	private static I2Cgyro instance;
-
+	private java.util.Timer integratorThread;
 	private I2Cgyro() {
 		gyro = new I2C(I2C.Port.kOnboard, address); // gyro is on board I2C port
 													// with address
 		gyro.write(initializeAddress, initializeValue);// used to initialize the gyro
+		integratorThread = new Timer();
+		integratorThread.schedule(new Integrator(), 0L,(long) (period*1000));
+		lasttime = System.currentTimeMillis();
 	}
 
 	public static I2Cgyro getInstance() {
@@ -29,18 +38,20 @@ public class I2Cgyro {
 		}
 		return instance;
 	}
-	public void update() {// called every period seconds
+	private void update() {// called every period seconds
 		byte buf[] = new byte[2]; // buffer for the data
 		SmartDashboard.putBoolean("GettingData?", gyro.read(zRateAddress, 2, buf)); // read the zaxis high and low bytes
 		//SmartDashboard.putNumber("idx", (double) idx++);
 		zRate = (short) ((buf[0] & 0xFF) << 8 | buf[1] & 0xFF); // convert to a
 																// short		
-		SmartDashboard.putNumber("Temp Z", zRate);
 		synchronized (this) {
 			ZRotateRate = (double) zRate / sensitivity; // convert to double in
 														// degrees/second
-			zAngle += ZRotateRate * period;// add the rate*dt to angle
+			if(Math.abs(ZRotateRate) < deadzone)
+				ZRotateRate = 0;
+			zAngle += ZRotateRate * (System.currentTimeMillis()-lasttime)/1000;// add the rate*dt to angle
 											// (integration)
+			lasttime = System.currentTimeMillis();
 		}
 	}
 
@@ -50,5 +61,14 @@ public class I2Cgyro {
 
 	public synchronized double getHeading() {
 		return zAngle;
+	}
+	private class Integrator extends TimerTask{
+
+		@Override
+		public void run() {
+			update();
+			
+		}
+		
 	}
 }
