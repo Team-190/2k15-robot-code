fb5e1d86d759b055aaaa74f19c0bf591db27a44d
Patrick <pmurphy2@wpi.edu>
2015-02-06 15:29:27 -0500

    Comments for I2C gyro added

--- FRC2k15/src/org/usfirst/frc190/FRC2k15/Components/I2Cgyro.java
+++ FRC2k15/src/org/usfirst/frc190/FRC2k15/Components/I2Cgyro.java
@@ -28,47 +28,49 @@ public class I2Cgyro {
 													// with address
 		gyro.write(initializeAddress, initializeValue);// used to initialize the gyro
 		integratorThread = new Timer();
-		integratorThread.schedule(new Integrator(), 0L,(long) (period*1000));
 		lasttime = System.currentTimeMillis();
+		integratorThread.schedule(new Integrator(), 0L,(long) (period*1000));
 	}
 
-	public static I2Cgyro getInstance() {
+	public static I2Cgyro getInstance() {//prevents more than one instance of the class from existing
 		if (instance == null) {
-			instance = new I2Cgyro();
+			instance = new I2Cgyro();//if an instance does not already exist create one
 		}
-		return instance;
+		return instance; //return the instance so other files can use it
 	}
 	private void update() {// called every period seconds
 		byte buf[] = new byte[2]; // buffer for the data
 		SmartDashboard.putBoolean("GettingData?", gyro.read(zRateAddress, 2, buf)); // read the zaxis high and low bytes
-		//SmartDashboard.putNumber("idx", (double) idx++);
 		zRate = (short) ((buf[0] & 0xFF) << 8 | buf[1] & 0xFF); // convert to a
 																// short		
 		synchronized (this) {
 			ZRotateRate = (double) zRate / sensitivity; // convert to double in
 														// degrees/second
-			if(Math.abs(ZRotateRate) < deadzone)
-				ZRotateRate = 0;
+			if(Math.abs(ZRotateRate) < deadzone)//if absolute value is less then deadzone
+				ZRotateRate = 0; //attempt to limit drift when not moving
 			zAngle += ZRotateRate * (System.currentTimeMillis()-lasttime)/1000;// add the rate*dt to angle
 											// (integration)
 			lasttime = System.currentTimeMillis();
 		}
 	}
 
-	public synchronized double getZRate() {
+	public synchronized double getZRate() {//gets the rate of rotation
 		return ZRotateRate;
 	}
 
-	public synchronized double getHeading() {
+	public synchronized double getHeading() {//gets the angle of rotation
 		return zAngle;
 	}
-	private class Integrator extends TimerTask{
+	private class Integrator extends TimerTask{//Timer to run integration routine
 
 		@Override
 		public void run() {
-			update();
+			update();//runs the integrator every period seconds
 			
 		}
 		
+	}
+	public synchronized void resetGyro(){//resets the angle on the gyro
+		zAngle = 0;
 	}
 }
