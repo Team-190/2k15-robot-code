9f42c346645a7945f405de5b2032b135cdf21c6e
tucker3896 <tucker3896@gmail.com>
2015-04-23 07:02:36 -0400

    Updated code for wings of doom full operaion

--- FRC2k15/src/org/usfirst/frc190/FRC2k15/commands/AutoWingsOfDoom.java
+++ FRC2k15/src/org/usfirst/frc190/FRC2k15/commands/AutoWingsOfDoom.java
@@ -8,8 +8,8 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package org.usfirst.frc190.FRC2k15.commands;
+
 import org.usfirst.frc190.FRC2k15.Robot;
 import org.usfirst.frc190.FRC2k15.Components.LimitedPIDSubsystemSetpointCommand;
 
@@ -19,36 +19,42 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 
-// Robot starts backward. Remember this when writing commands for driving in auto.
+// Robot starts backward. Remember this when writing commands for driving in
+// auto.
 // Chainsaw should start all the way back
 
-
-// Remember to write a PID command to tilt chainsaw to vertical position after the DisableWings() command.
-
+// Remember to write a PID command to tilt chainsaw to vertical position after
+// the DisableWings() command.
 
 public class AutoWingsOfDoom extends CommandGroup {
-    
-    public  AutoWingsOfDoom() {
- 
-  	 
-	addSequential(new FireWings());
-//    	addParallel(new ResetChainsaw());
-	addSequential (new Delay (1));
-   	addSequential(new DisableWings());
-   	addSequential (new Delay (2));
- 	addSequential(new DriveDistDirection(0.40,30,0));
-//    	Tote align position is best angle. this brings wings up
-//   	addSequential(new Delay(1.5));
-    	addSequential(new LimitedPIDSubsystemSetpointCommand(Robot.chainsaw4Bar, Robot.chainsaw4Bar.scoringPosition, 0.4, -0.4, false)); 
-    	addSequential(new LimitedPIDSubsystemSetpointCommand(Robot.chainsaw4Bar, Robot.chainsaw4Bar.collectPositionWide, 0.4, -0.4, false)); 
-    	addSequential(new LimitedPIDSubsystemSetpointCommand(Robot.chainsaw4Bar, Robot.chainsaw4Bar.scoringPosition, 0.4, -0.4, false)); 
-    	addSequential(new LimitedPIDSubsystemSetpointCommand(Robot.chainsaw4Bar, Robot.chainsaw4Bar.collectPositionWide, 0.4, -0.4, false)); 
-    	
-//   	Rest of the commands are for driving over and picking up two totes
-//    	addSequential(new DriveDistDirection(.8, 53, 90));
-//    	addSequential(new DriveTurn(180));
-//    	addSequential(new AutoTotePickupWide());
-    	
 
-    }
+	public AutoWingsOfDoom() {
+
+		addParallel(new FireWingsSequence());
+		// addParallel(new ResetChainsaw());
+		// addSequential(new Delay(1));
+		addSequential(new DriveDistDirection(1.0, 24, 0));
+		addSequential(new DriveDistDirection(0.4, 24, 0));
+
+		// Tote align position is best angle. this brings wings up
+		// addSequential(new Delay(1.5));
+		addSequential(new LimitedPIDSubsystemSetpointCommand(
+				Robot.chainsaw4Bar, Robot.chainsaw4Bar.scoringPosition, 0.4,
+				-0.4, false));
+		addSequential(new LimitedPIDSubsystemSetpointCommand(
+				Robot.chainsaw4Bar, Robot.chainsaw4Bar.collectPositionWide,
+				0.4, -0.4, false));
+		addSequential(new LimitedPIDSubsystemSetpointCommand(
+				Robot.chainsaw4Bar, Robot.chainsaw4Bar.scoringPosition, 0.4,
+				-0.4, false));
+		addSequential(new LimitedPIDSubsystemSetpointCommand(
+				Robot.chainsaw4Bar, Robot.chainsaw4Bar.collectPositionWide,
+				0.4, -0.4, false));
+
+		// Rest of the commands are for driving over and picking up two totes
+		// addSequential(new DriveDistDirection(.8, 53, 90));
+		// addSequential(new DriveTurn(180));
+		// addSequential(new AutoTotePickupWide());
+
+	}
 }
